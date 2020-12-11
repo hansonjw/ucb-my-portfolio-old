d27c53eb225ed5da4e13a8dd0e7cbb1589be8715
hansonjw <hansonjw@gmail.com>
2020-12-11 16:55:58 -0600

    Add portfolio page content including pictures, etc.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,14 +1,70 @@
 import React from 'react';
 import annie1 from "../../assets/annie1.jpeg";
+import anImage from "../../assets/me.jpeg";
+
+import investing from '../../assets/investing.png';
+import note from '../../assets/note.png';
+import blog from '../../assets/blog.png';
+import run from '../../assets/run.png';
+import stocks from '../../assets/stocks.png';
+import weather from '../../assets/weather.png';
 
 function Portfolio() {
 
+    const projects = [
+        {   
+            id: 'investing',
+            url: 'https://seedstocks.herokuapp.com/',
+            git: 'https://github.com/hansonjw/stock-quotes',
+            image: investing
+        },
+        {
+            id: 'note',
+            url: 'https://lit-beach-81656.herokuapp.com/',
+            git: 'https://github.com/hansonjw/mini-eureka',
+            image: note
+        },
+        {
+            id: 'blog',
+            url: 'https://vast-fjord-84705.herokuapp.com/',
+            git: 'https://github.com/hansonjw/tech-blog',
+            image: blog
+        },
+        {
+            id: 'run',
+            url: 'https://hansonjw.github.io/run-buddy/',
+            git: 'https://github.com/hansonjw/run-buddy',
+            image: run
+        },
+        {
+            id: 'stocks',
+            url: 'https://github.com/aadi1988/Common-Capitalism',
+            git: 'https://aadi1988.github.io/Common-Capitalism/',
+            image: stocks
+        },
+        {
+            id: 'weather',
+            url: 'https://hansonjw.github.io/weatherdashboard/',
+            git: 'https://github.com/hansonjw/weatherdashboard',
+            image: weather
+        }
+    ]
+
     return(
-        <section>
-            <div>this is the PORTFOLIO section</div>
+        <div>
+            {projects.map((project) => (
+                <div>
+                    <img src={project.image} />
+                    <a href={project.url}>link to site</a>
+                    <a href={project.git}>github repo</a>
+                </div>
+            ))}
             <img src={annie1} />
-        </section>
+            <img src={anImage} />
+        </div>
     )
 }
 
 export default Portfolio;
+
+
